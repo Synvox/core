7b543cfb0c3a5485ba7a85d091679eacebbce196
Ryan Allred <ryanallredxyz@gmail.com>
2021-03-29 22:27:39 -0600

    Add test for deep validations

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -26,4 +26,36 @@ describe("validation", () => {
       }
     `);
   });
+
+  it("validates deep", async () => {
+    const cast = await validate<{ prop: string }>(
+      object({ prop: object({ prop: string() }) }),
+      {
+        prop: { prop: 1 },
+      }
+    );
+
+    expect(cast).toMatchInlineSnapshot(`
+      Object {
+        "prop": Object {
+          "prop": "1",
+        },
+      }
+    `);
+  });
+
+  it("validates basic (failure)", async () => {
+    const cast = await validate(object({ prop: object({ prop: string() }) }), {
+      prop: { prop: [] },
+    }).catch((e) => e.body);
+    expect(cast).toMatchInlineSnapshot(`
+      Object {
+        "errors": Object {
+          "prop": Object {
+            "prop": "must be a \`string\` type, but the final value was: \`[]\`.",
+          },
+        },
+      }
+    `);
+  });
 });
